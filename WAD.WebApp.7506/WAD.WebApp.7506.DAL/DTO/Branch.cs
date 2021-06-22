cd1524e011b5a859766394442c95613c4bf15de6
00007506 <sadinovs123@gmail.com>
2021-06-22 14:52:10 +0500

    Model classes created, Branch and Employee

---
+++ WAD.WebApp.7506/WAD.WebApp.7506.DAL/DTO/Branch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAD.WebApp._7506.DAL.DTO
+{
+    public class Branch
+    {
+        public int BranchId { get; set; }
+
+        [DisplayName("Branch Name")]
+        public string BranchName { get; set; }
+
+        [DisplayName("Branch Email")]
+        public string BranchEmail { get; set; }
+
+        [DisplayName("Branch Address")]
+        public string BranchAddress { get; set; }
+    }
+}
---
+++ WAD.WebApp.7506/WAD.WebApp.7506.DAL/DTO/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAD.WebApp._7506.DAL.DTO
+{
+    public class Employee
+    {
+        [DisplayName("Id")]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        public string Address { get; set; }
+
+        [Required]
+        [MinLength(9)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public string Position { get; set; }
+
+        [Required]
+        [Display(Name = "Branch")]
+        public int BranchId { get; set; }
+
+        public Branch Branch { get; set; }
+
+    }
+}

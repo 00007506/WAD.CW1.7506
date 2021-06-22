0349a3e4e6aecdda4570b3209190e31a9f239b60
00007506 <sadinovs123@gmail.com>
2021-06-22 15:08:17 +0500

    DbContext created

---
+++ WAD.WebApp.7506/WAD.WebApp.7506.DAL/CarRentDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WAD.WebApp._7506.DAL.DTO;
+
+namespace WAD.WebApp._7506.DAL
+{
+    public class CarRentDbContext : DbContext
+    {
+        public CarRentDbContext(DbContextOptions<CarRentDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        public virtual DbSet<Branch> Branch { get; set; }
+    }
+}
